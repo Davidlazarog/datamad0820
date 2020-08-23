af237997f0f2161a8621e22a4913a35fb8d43d43
David Lazaro <david@MacBook-Air-de-David.local>
2020-08-23 22:49:19 +0200

    Travesura realiada

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -38,8 +38,9 @@ class Viking (Soldier):
 # Saxon
 
 
-class Saxon:
+class Saxon (Soldier):
     def __init__(self, health, strength):
+        super().__init__(health,strength)
         self.health = health
         self.strength = strength
     def attack (self):  
@@ -55,7 +56,42 @@ class Saxon:
          
 # War
 
+class War ():
 
-class War:
+    vikingArmy = []
+    saxonArmy = []
 
-    pass
+    def __init__():
+        super().__init__(self)
+        self.vikingArmy = vikingArmy
+        self.saxonArmy = saxonArmy
+
+
+    def addViking(self , Viking):
+        self.vikingArmy.append(super(),name())
+
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(self.Saxon)
+        
+    def vikingAttack(self):
+        self.Viking.health = self.Saxon.receiveDamage
+        if self.Viking.health < 0 :
+            vikingArmy.remove(self.Viking)
+        return self.Saxon.receiveDamage(self.Viking.strength)
+
+    def saxonAttack(self):
+        elf.Saxon.health = self.Viking.receiveDamage
+        if self.Saxon.health < 0 :
+            saxonArmy.remove(self.Saxon)
+        return self.Viking.receiveDamage(self.Saxon.strength)
+ 
+    def showStatus(self):
+        if vikingArmy <= 0:
+            return "Saxons have fought for their lives and survive another day..."
+        elif saxonArmy <= 0:
+            return "Vikings have won the war of the century!"
+        elif saxonArmy >= 0 and Viking >= 0:
+            return "Vikings and Saxons are still in the thick of battle."
+
+
+    
